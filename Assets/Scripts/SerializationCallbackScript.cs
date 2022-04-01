24ba87f382da8d61c0e4db01ec0c9a7e77c38a2f
Vlad <65783732+VladTheCreator@users.noreply.github.com>
2022-04-01 11:05:09 +0200

    Initial commit

---
+++ Assets/Scripts/SerializationCallbackScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerializationCallbackScript : MonoBehaviour, ISerializationCallbackReceiver
+{
+   public void OnBeforeSerialize()
+	{
+		
+	}
+	public void OnAfterDeserialize()
+	{
+
+	}
+}
